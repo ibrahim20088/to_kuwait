9695d192cecba438f9f164e4b2e426540f99bc5a
ibrahim20088 <87567802+ibrahim20088@users.noreply.github.com>
2023-06-24 11:49:31 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,27 +1,21 @@
-const days = document.getElementById('days');
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
-
-const currentYear = new Date().getFullYear();
-
-const newYearTime = new Date(` June 24 , 2023 23:55:00 
-`);
-
-// Update countdown time
 function updateCountdown() {
-	const currentTime = new Date();
-	const diff = newYearTime - currentTime;
+    const currentTime = new Date();
+    const diff = newYearTime - currentTime;
 
-	const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-	const h = Math.floor(diff / 1000 / 60 / 60) % 24;
-	const m = Math.floor(diff / 1000 / 60) % 60;
-	const s = Math.floor(diff / 1000) % 60;
+    if (diff <= 0) {
+        window.location.href = "https://ibrahim20088.github.io/ibrahimfun/";
+        return; // Exit the function to stop further countdown updates
+    }
 
-	days.innerHTML = d;
-	hours.innerHTML = h < 10 ? '0' + h : h;
-	minutes.innerHTML = m < 10 ? '0' + m : m;
-	seconds.innerHTML = s < 10 ? '0' + s : s;
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
 }
 
 setInterval(updateCountdown, 1000);
